062f1f03e568f2927a9c1c07ff1b9698d870a564
aniketmahajan04 <mahajananiket207@gmail.com>
2026-06-25 11:03:13 +0530

    fix error lense issue

--- init.lua
+++ init.lua
@@ -5,3 +5,21 @@ require("config.plugins.terminal")
 
 vim.opt.laststatus = 0
 vim.opt.cmdheight = 1
+
+vim.diagnostic.config({
+				virtual_text = {
+					prefix = "●",
+					source = "if_many",
+				},
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+				float = {
+					focusable = true,
+					border = "rounded",
+					source = true,
+					header = "",
+					prefix = "",
+				},
+			})
